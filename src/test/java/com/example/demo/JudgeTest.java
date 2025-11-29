583b2ff21f268fe58ff1d9283c7d6f9a9a07892a
yamakoala <52974545+mettokoala@users.noreply.github.com>
2025-11-29 18:16:26 +0900

    testプロファイル、assumeTrue、assumingThat

--- src/test/java/com/example/demo/JudgeTest.java
+++ src/test/java/com/example/demo/JudgeTest.java
@@ -1,19 +1,29 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class JudgeTest {
+
+	@Value("${test.enabled}")
+	boolean testEnabled;
 
 	static Stream<Arguments> judgeArugments() {
 		return Stream.of(
 				Arguments.of(new Judge("AA", 10), false),
-				Arguments.of(new Judge("BA", 11), true),
+				Arguments.of(new Judge("BA", 11), false),
 				Arguments.of(new Judge("AA", 11), true));
 	}
 
@@ -23,4 +33,17 @@ class JudgeTest {
 		assertEquals(expected, judge.isApprove());
 	}
 
+	@Test
+	void testAssumeTrue() {
+		assumeTrue(testEnabled, "falseなのでスキップしました:" + testEnabled);
+		System.out.println("assumeTrue:trueなので実行されました");
+	}
+
+	@Test
+	void testAssumingThat() {
+		assumingThat(testEnabled, () -> {
+			System.out.println("assumingThat:trueなので実行されました");
+		});
+		System.out.println("assumingThat:常に実行されます");
+	}
 }
