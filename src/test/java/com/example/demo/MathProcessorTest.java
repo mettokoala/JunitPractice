e62c906500e4bc3612740301df29c30de2fae717
yamakoala <52974545+mettokoala@users.noreply.github.com>
2026-02-22 15:55:03 +0900

    verify、inOrder、引数マッチング

--- src/test/java/com/example/demo/MathProcessorTest.java
+++ src/test/java/com/example/demo/MathProcessorTest.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -24,25 +26,32 @@ class MathProcessorTest {
 
 	@Test
 	void testAddAndDouble() {
-		doReturn(10).when(calculator).add(2, 3);
+		doReturn(10).when(calculator).add(anyInt(), anyInt());
 		int actual = mathProcessor.addAndDouble(2, 3);
 		assertEquals(20, actual);
+		verify(calculator).add(anyInt(), anyInt());
 	}
 
 	@Test
 	void testDivideAndDouble() {
-		doThrow(new IllegalArgumentException("0で割れません")).when(calculator).divide(10, 0);
+		doThrow(new IllegalArgumentException("0で割れません")).when(calculator).divide(anyInt(), eq(0));
 		int actual = mathProcessor.divideAndDouble(10, 0);
 		assertEquals(-2, actual);
+		verify(calculator, atLeastOnce()).divide(anyInt(), eq(0));
+		verify(judge, never()).isApprove();
 	}
 
 	@Test
 	void testNotifyAndApprove() {
-		doNothing().when(judge).notifyResult("OK");
+		doNothing().when(judge).notifyResult(anyString());
 		when(judge.isApprove()).thenReturn(true);
 
 		boolean actual = mathProcessor.notifyAndApprove("OK");
 		assertTrue(actual);
+
+		InOrder inOrder = inOrder(judge);
+		inOrder.verify(judge).notifyResult(anyString());
+		inOrder.verify(judge).isApprove();
 	}
 
 	@Test
@@ -51,9 +60,11 @@ class MathProcessorTest {
 			int a = inv.getArgument(0);
 			int b = inv.getArgument(1);
 			return a + b + 100;
-		}).when(calculator).add(1, 2);
+		}).when(calculator).add(anyInt(), anyInt());
 
 		int actual = mathProcessor.addAndDouble(1, 2);
 		assertEquals(206, actual);
+
+		verify(calculator).add(intThat(a -> a > 0), intThat(b -> b > 0));
 	}
 }
