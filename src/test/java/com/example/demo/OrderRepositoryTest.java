169391831a1360ae74c0ded3f5bcbee0a9d6b85e
yamakoala <52974545+mettokoala@users.noreply.github.com>
2026-05-02 19:20:08 +0900

    DBUnitでDB状態を比較する

--- src/test/java/com/example/demo/OrderRepositoryTest.java
+++ src/test/java/com/example/demo/OrderRepositoryTest.java
@@ -2,6 +2,21 @@ package com.example.demo;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.InputStream;
+import java.sql.SQLException;
+
+import org.dbunit.Assertion;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.filter.DefaultColumnFilter;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.h2.H2DataTypeFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +39,31 @@ class OrderRepositoryTest {
 	}
 
 	@Test
-	void test() {
+	void test() throws DatabaseUnitException, SQLException {
 		orderRepository.save("りんご", 10);
 
-		Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM order_info", Integer.class);
-		assertThat(count).isEqualTo(1);
+		try (var con = jdbcTemplate.getDataSource().getConnection()) {
+			IDatabaseConnection dbUnitCon = new DatabaseConnection(con);
+
+			DatabaseConfig config = dbUnitCon.getConfig();
+			config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
+
+			IDataSet actualDataSet = dbUnitCon.createDataSet();
+			ITable actual = actualDataSet.getTable("ORDER_INFO");
+			ITable actualFilter = DefaultColumnFilter.includedColumnsTable(actual,
+					new String[] { "ITEM_NAME", "QUANTITY" });
+
+			String xmlPass = "/expected.xml";
+			ITable expected = readDataSet(xmlPass).getTable("ORDER_INFO");
+
+			Assertion.assertEquals(expected, actualFilter);
+		}
+	}
+
+	private FlatXmlDataSet readDataSet(String xmlPass) throws DataSetException {
+		InputStream is = getClass().getResourceAsStream(xmlPass);
+		assertThat(is).isNotNull();
+		return new FlatXmlDataSetBuilder().build(is);
 	}
 
 }
